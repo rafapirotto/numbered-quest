c71259f649f87258a65d0bcbcc215db32b5fa78b
Rafael Pirotto <rafael.pirotto@liongard.com>
2023-11-08 11:32:49 -0300

    Added missing types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React, { ReactElement, useEffect, useRef } from 'react';
 import { Wrapper, Status } from '@googlemaps/react-wrapper';
 
+import './App.css';
+
 const render = (status: Status): React.ReactElement => {
   if (status === Status.FAILURE) return <div>Error loading map</div>;
   return <div>Loading...</div>;
+};
+
+let label = 1;
+
+const addMarker = (
+  location: google.maps.LatLngLiteral,
+  map: google.maps.Map,
+): void => {
+  new google.maps.Marker({
+    position: location,
+    label: String(label++),
+    map: map,
+  });
 };
 
 const MyMapComponent = ({
@@ -12,23 +27,26 @@ const MyMapComponent = ({
 }: {
   center: google.maps.LatLngLiteral;
   zoom: number;
-}) => {
+}): ReactElement => {
   const mapRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (mapRef.current) {
-      new window.google.maps.Map(mapRef.current, {
+      const map = new window.google.maps.Map(mapRef.current, {
         center,
         zoom,
+      });
+      google.maps.event.addListener(map, 'click', (event) => {
+        addMarker(event.latLng, map);
       });
     }
   }, [center, zoom]);
 
-  return <div ref={mapRef} style={{ height: '400px', width: '100%' }} />;
+  return <div className="map" ref={mapRef} />;
 };
 
 const App = (): ReactElement => {
-  const center = { lat: -34.397, lng: 150.644 };
+  const center = { lat: -25.363, lng: 131.044 };
   const zoom = 4;
 
   return (
